eddc8e984d3a9c75e6fa6e592154c4bda237f332
Rishabh Singh <rishabhsingh3842@gmail.com>
2026-08-14 12:46:33 +0530

    Add: sort-an-array - submission-1

---
+++ Data Structures & Algorithms/sort-an-array/submission-1.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    int partition(vector<int>& nums, int left, int right) {
+
+        int pivot = nums[right];
+        int i = left;
+
+        for (int j = left; j < right; j++) {
+
+            if (nums[j] < pivot) {
+                swap(nums[i], nums[j]);
+                i++;
+            }
+        }
+
+        swap(nums[i], nums[right]);
+
+        return i;
+    }
+
+    void quickSort(vector<int>& nums, int left, int right) {
+
+        if (left >= right)
+            return;
+
+        int pivotIndex = partition(nums, left, right);
+
+        quickSort(nums, left, pivotIndex - 1);
+        quickSort(nums, pivotIndex + 1, right);
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+
+        quickSort(nums, 0, nums.size() - 1);
+
+        return nums;
+    }
+};
